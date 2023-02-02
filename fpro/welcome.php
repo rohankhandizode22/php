897e6dec1b0c3727de49395cc1e4e4449d9ac5fd
rohankhandizode22 <124252279+rohankhandizode22@users.noreply.github.com>
2023-02-02 15:54:59 +0530

    Add files via upload

---
+++ fpro/welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome Page</title>
+    </head>
+    <body>
+        <h2> Welcome User . You are registered</h2>
+        <h5> Go to login page <a href="login.php"> Click Here </a> </h5>
+    </body>
+</html>
